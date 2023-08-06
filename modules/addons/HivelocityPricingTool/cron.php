1f4c52a3fad026ed513ecdc1b8169a52088998df
Ahmadian <ahmadian@asamserver.com>
2023-08-06 12:30:12 +0330

    remove some of codes, i revers this change in future

--- modules/addons/HivelocityPricingTool/cron.php
+++ modules/addons/HivelocityPricingTool/cron.php
@@ -1,25 +1,20 @@
 <?php
 
-require dirname(dirname(dirname(__DIR__))) . '/init.php';
+require dirname(__DIR__, 3) . '/init.php';
 require_once 'Autoloader.php';
+
+use HivelocityPricingTool\classes\Cron;
 
 try {
     logModuleCall('high', 'cron', 'first', $argv);
-    \HivelocityPricingTool\classes\Cron::priceChangeNotify();
-    parse_str($argv[1], $params);
-    if ($params['fivemincron']) {
-        $q = mysql_query("SELECT value FROM mod_hivelocity_cron WHERE value='RunFiveMinCron'");
-        if (mysql_num_rows($q)) {
-            logModuleCall('high', 'cron', '2nd', $params);
-            \HivelocityPricingTool\classes\Cron::synchronizeProducts();
-            mysql_query("DELETE FROM mod_hivelocity_cron WHERE value='RunFiveMinCron'");
-        }
-    } else {
-        logModuleCall('high', 'cron', '3rd', 'once a day');
-        \HivelocityPricingTool\classes\Cron::synchronizeProducts();
-    }
+
+    Cron::priceChangeNotify();
+
+    logModuleCall('high', 'cron', '2nd', 'cron job setting');
+
+    Cron::synchronizeProducts();
 } catch (Exception $e) {
     $loggedWhmcsUserId = $_SESSION["uid"];
+
     logActivity("HivelocityPricingTool: " . $e->getMessage(), $loggedWhmcsUserId);
 }
-
