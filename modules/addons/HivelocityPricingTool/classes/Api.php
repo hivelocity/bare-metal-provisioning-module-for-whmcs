cb8a46028938e13a40cee341c93bd0fb4e9a1154
Ahmadian <ahmadian@asamserver.com>
2023-08-06 12:37:29 +0330

    refactoring and modify codes

--- modules/addons/HivelocityPricingTool/classes/Api.php
+++ modules/addons/HivelocityPricingTool/classes/Api.php
@@ -3,6 +3,9 @@
 namespace HivelocityPricingTool\classes;
 
 use Exception;
+use Throwable;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 
 class Api
 {
@@ -22,154 +25,280 @@ class Api
         $apiKey = self::$apiKey;
         $url = self::$apiUrl . $resource;
 
-        $ch = curl_init();
+        $data = [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Accept' => '*/*',
+                'Accept-Encoding' => '',
+                'X-API-KEY' => $apiKey,
+                'Referer' => 'WHMCS',
+            ],
+        ];
 
         if (!empty($postFields)) {
             if ($httpMethod == "POST" && $postInQuery) {
-                $postQuery = http_build_query($postFields);
-                $url = $url . "?" . $postQuery;
+                $data['query'] = $postFields;
             } else {
-                $postJson = json_encode($postFields);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $postJson);
+                $data['body'] = json_encode($postFields);
             }
         }
 
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_FAILONERROR, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $httpMethod);
+        try {
+            $request = (new Client)->request($httpMethod, $url, $data);
 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            "Content-Type: application/json",
-            "User-Agent: PostmanRuntime/7.26.8",
-            "Accept: */*",
-            "Accept-Encoding: ''",
-            "X-API-KEY: $apiKey",
-            "Referer: WHMCS"
-        ]);
+            $response = json_decode($request->getBody(), true);
 
-        $response = curl_exec($ch);
-        $cUrlError = curl_error($ch);
-        curl_close($ch);
-
-        if (1) {
-            if ($cUrlError) {
-                $response = $cUrlError;
-            }
-
-            $action = "Hivelocity API Call";
-            $request = [
-                "Resource" => $resource,
-                "Method" => $httpMethod,
-                "Fields" => !empty($postFields) ? $postFields : "",
-                "Json" => !empty($postJson) ? $postJson : "",
-                "Query" => !empty($postQuery) ? $postQuery : "",
-            ];
-        }
-
-        if ($cUrlError) {
-            throw new Exception($cUrlError);
-        }
-
-        $rawResponse = $response;
-        if (!empty($response)) {
-            $response = json_decode($response, true);
-
-            if (json_last_error() != JSON_ERROR_NONE) {
-                throw new Exception("API response is invalid. " . $rawResponse);
-            }
-
-            if (isset($response["message"]) && isset($response["code"]) && $response["code"] != 201) {
-                if (is_array($response["message"])) {
-                    $message = implode(" ", $response["message"]);
+            if (isset($response['message']) && isset($response['code']) && $response['code'] != 201) {
+                if (is_array($response['message'])) {
+                    $message = implode(' ', $response['message']);
                 } else {
-                    $message = $response["message"];
+                    $message = $response['message'];
                 }
 
                 throw new Exception($message);
             }
 
-            if (isset($response["description"]) && isset($response["code"]) && $response["code"] > 201) {
-                if (is_array($response["description"])) {
-                    $message = implode(" ", $response["description"]);
+            if (isset($response['description']) && isset($response['code']) && $response['code'] > 201) {
+                if (is_array($response['description'])) {
+                    $message = implode(' ', $response['description']);
                 } else {
-                    $message = $response["description"];
+                    $message = $response['description'];
                 }
 
                 throw new Exception($message);
             }
 
-            if (isset($response["schema_errors"])) {
-                $fullMessage = "";
-                foreach ($response["schema_errors"] as $field => $error) {
+            if (isset($response['schema_errors'])) {
+                $fullMessage = '';
+                foreach ($response['schema_errors'] as $field => $error) {
                     foreach ($error as $msg) {
                         if (is_array($msg)) {
-                            $message = implode(" ", $msg);
+                            $message = implode(' ', $msg);
                         } else {
                             $message = $msg;
                         }
-                        $message = $field . ": " . $message;
+                        $message = $field . ': ' . $message;
                     }
-                    $fullMessage .= $message . " ";
+                    $fullMessage .= $message . ' ';
                 }
                 throw new Exception($fullMessage);
             }
 
-            if (isset($response["errors"])) {
-                $fullMessage = "";
-                foreach ($response["errors"] as $field => $error) {
+            if (isset($response['errors'])) {
+                $fullMessage = '';
+                foreach ($response['errors'] as $field => $error) {
                     if (is_array($error)) {
-                        $message = $field . ": " . implode(" ", $error);
+                        $message = $field . ': ' . implode(' ', $error);
                         $fullMessage .= $message . " ";
                     } else {
                         $message = $error;
-                        $fullMessage .= $message . " ";
+                        $fullMessage .= $message . ' ';
                     }
                 }
                 throw new Exception($fullMessage);
             }
-        } else {
-            throw new Exception("API response is invalid.");
-        }
 
-        return $response;
+            return $response;
+
+        }catch (ClientException|Throwable $e){
+            throw new Exception($e->getMessage(), $e->getCode());
+        }
     }
 
     public static function getProductList()
     {
         $resource = "/inventory/product";
+        $response = self::sendRequest($resource);
 
-        return self::sendRequest($resource);
+        return $response;
     }
 
     public static function getProductDetails($productId)
     {
         $resource = "/inventory/product/$productId";
+        $response = self::sendRequest($resource);
 
-        return self::sendRequest($resource);
+        return $response;
     }
 
     public static function getProductOptions($productId)
     {
         $resource = "/product/$productId/options?groupBy=upgrade";
+        $response = self::sendRequest($resource);
 
-        return self::sendRequest($resource);
+        return $response;
     }
 
     public static function getProductOS($productId)
     {
         $resource = "/product/$productId/operating-systems";
+        $response = self::sendRequest($resource);
 
-        return self::sendRequest($resource);
+        return $response;
     }
 
     public static function getBillingInfoList()
     {
         $resource = "/billing-info/";
+        $response = self::sendRequest($resource);
 
-        return self::sendRequest($resource);
+        return $response;
+    }
+
+    public static function createDeployment($deploymentName)
+    {
+        $resource = "/deploy/";
+
+        $postFields = [
+            "deploymentName" => $deploymentName,
+        ];
+
+        $response = self::sendRequest($resource, "POST", $postFields, true);
+
+        return $response;
+    }
+
+    public static function getDeploymentList()
+    {
+        $resource = "/deploy/";
+
+        $response = self::sendRequest($resource);
+
+        return $response;
+    }
+
+    public static function getDeploymentDetails($deploymentId)
+    {
+        $resource = "/deploy/$deploymentId";
+
+        $response = self::sendRequest($resource);
+
+        return $response;
+    }
+
+    public static function configureDeployment($deploymentId, $productId, $locationId, $osId, $panelId, $hostName,
+        $billingPeriod
+    )
+    {
+        $resource = "/deploy/$deploymentId";
+
+        $postFields = [
+            "locationCode" => $locationId,
+            "billingPeriod" => $billingPeriod,
+            "operatingSystem" => $osId,
+            "hostnames" => [$hostName],
+            "productId" => $productId,
+        ];
+
+        if (!empty($panelId)) {
+            $postFields["options"] = [$panelId];
+        }
+
+        $response = self::sendRequest($resource, "PUT", $postFields);
+
+        return $response;
+    }
+
+    public static function executeDeployment($deploymentId, $billingInfoId)
+    {
+        $resource = "/deploy/$deploymentId";
+
+        $postFields = [
+            "billingInfo" => $billingInfoId,
+        ];
+
+        $response = self::sendRequest($resource, "POST", $postFields);
+
+        return $response;
+    }
+
+    public static function getInvoiceList()
+    {
+        $resource = "/invoice/";
+
+        $response = self::sendRequest($resource);
+
+        return $response;
+    }
+
+    public static function getOrderList()
+    {
+        $resource = "/order/";
+
+        $response = self::sendRequest($resource);
+
+        return $response;
+    }
+
+    public static function getServiceList($orderId)
+    {
+        $resource = "/service/?orderId=$orderId";
+
+        $response = self::sendRequest($resource);
+
+        return $response;
+    }
+
+    public static function getDeviceList()
+    {
+        $resource = "/device/";
+
+        $response = self::sendRequest($resource);
+
+        return $response;
+    }
+
+    public static function getDeviceDetails($deviceId)
+    {
+        $resource = "/device/$deviceId";
+        $response = self::sendRequest($resource);
+
+        return $response;
+    }
+
+    public static function cancelDevice($deviceId)
+    {
+        $resource = "/cancellation/cancellation";
+
+        $postFields = [
+            "deviceId" => $deviceId,
+        ];
+
+        $response = self::sendRequest($resource, "POST", $postFields);
+
+        return $response;
+    }
+
+    public static function getGraph($deviceId, $period = "day", $start = null, $end = null)
+    {
+        $resource = "/bandwidth/device/$deviceId/image";
+
+        $postFields = [
+            "period" => $period,
+            "start" => $start,
+            "end" => $end,
+            "interface" => "eth0",
+        ];
+
+        $response = self::sendRequest($resource, "POST", $postFields, true);
+
+        return $response;
+    }
+
+    public static function getBandwidthDetails($deviceId, $period = "day", $start = null, $end = null)
+    {
+        $resource = "/bandwidth/device/$deviceId";
+
+        $postFields = [
+            "period" => $period,
+            "start" => $start,
+            "end" => $end,
+            "interface" => "eth0",
+        ];
+
+        $response = self::sendRequest($resource, "POST", $postFields, true);
+
+        return $response;
     }
 
     public static function executePowerAction($deviceId, $powerAction)
@@ -180,7 +309,9 @@ class Api
             "action" => $powerAction,
         ];
 
-        return self::sendRequest($resource, "POST", $postFields, true);
+        $response = self::sendRequest($resource, "POST", $postFields, true);
+
+        return $response;
     }
 
     public static function bootDevice($deviceId)
